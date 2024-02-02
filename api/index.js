2902cfbb90663a8c56482502fd4f8a3113dcf120
Daniell J. Bow <daniellbow@gmail.com>
2024-02-02 17:46:16 +0000

    add create listing api route

--- api/index.js
+++ api/index.js
@@ -1,12 +1,13 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import cookieParser from 'cookie-parser'
+import cookieParser from "cookie-parser";
 dotenv.config();
 
 // Routes
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
+import listingRouter from "./routes/listing.route.js";
 
 mongoose
   .connect(process.env.MONGO)
@@ -21,7 +22,7 @@ const app = express();
 
 app.use(express.json());
 
-app.use(cookieParser())
+app.use(cookieParser());
 
 const PORT = 3000;
 
@@ -31,6 +32,7 @@ app.listen(PORT, () => {
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+app.use("/api/listing", listingRouter);
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
